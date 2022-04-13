81431e7afa4efc99c8597cb372fd98bc20d6e09c
Gonzalo <gonzalovelazco32@gmail.com>
2022-04-13 10:17:54 -0300

    Importada la librería datetime, y finalizada la cración de los imputs de nombre y edad.

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,22 @@
+# Crea un programa que le pida al usuario que ingrese su nombre y su edad.
+# Imprime un menasje dirigido a ellos que les indique el año en que cumplirán 100 años 
+# 
+# Extras:
+# 1: Agregue al programa anterior pidiéndole al usuario otro número e imprimiendo tantas
+# copias del mensaje anterior. (Pista: El orden de las operaciones existe en Python).
+# 2: Imprima esa cantidad de copias del mensaje anterior en lineas separadas. (Pista: 
+# la cadena "\n" es lo mismo que precionar ENTER)
+
+from datetime import date, time, datetime
+
 def run():
-    pass
+    nombre = input('Bienvenido al recolector de edades y nombres, ingrese su nombre: ')
+    edad = int(input('Ingrese su edad: '))
+    fecha_actual = 
+
+    def cien_años():
+         = edad + 100
+
+
+if __name__ == '__main__':
+    run()
