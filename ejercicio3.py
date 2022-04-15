75cf5274e25326f4f36cec70d58c81121639deb8
Gonzalo H. Velazco <gonzalovelazco32@gmail.com>
2022-04-15 12:36:28 -0300

    Enumerated, and ordered, of the tasks.

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,13 +1,12 @@
-#Take a list, say for example this one:
-
-#  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-#and write a program that prints out all the elements of the list that are less than 5.
+# 1) Take a list, say for example this one:
+#       a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#   and write a program that prints out all the elements of the list that are less than 5.
 
 #Extras:
 
-#Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
-#Write this in one line of Python.
-#Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+# 2) Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+# 3) Write this in one line of Python.
+# 4) Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
 
 def run():
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
@@ -16,7 +15,7 @@ def run():
     new_list = []
     for i in a:
         if i < 5:
-            new_list.append(i)
+            new_list.append(i) #Second point of the exercice integrated to the first.            
     print(new_list)
 
 if __name__ == '__main__':
