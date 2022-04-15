a35075c6b362997413da8db58d219ad3a0d4b26c
Gonzalo H. Velazco <gonzalovelazco32@gmail.com>
2022-04-15 13:42:33 -0300

    Removed the 'new_list' and complete task 4 of exercise3

--- ejercicio3.py
+++ ejercicio3.py
@@ -9,15 +9,11 @@
 # 4) Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
 
 def run():
+    usr_num = int(input('Please, enter a number between 1 and 89: '))
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
-# First point of the exercice.
-# Second point of the exercice integrated to the first.
-
-# Third point integrated to the second and the first point:
-    new_list = []
-    n_list = [new_list.append(i) for i in a if i < 5]
-    print(new_list)
+    #new_list = []
+    n_list = [i for i in a if i < usr_num]
+    print(n_list)
 
 if __name__ == '__main__':
     run()
