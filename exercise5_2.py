7dd51b4d47931c8cd1dfc95cd5c775c7e5e34ade
Gonzalo H. Velazco <gonzalovelazco32@gmail.com>
2022-04-15 19:24:39 -0300

    Working on random lists.

---
+++ exercise5_2.py
@@ -0,0 +1,33 @@
+# 1)Take two lists, say for example these two:
+
+#  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+#Extras:
+
+# 2)Randomly generate two lists to test this
+# 3)Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+
+import random
+
+def run():
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    com = []
+    ran_1 = []
+    ran_2 = []
+
+    # for i in b:
+    #      if i == i in a:
+    #          com.append(i)
+    # print(com)
+
+    val = random.randint(1, 100)
+    
+    for i in val:
+        ran_1.append(i)
+    print(ran_1)
+
+if __name__ == '__main__':
+    run()
