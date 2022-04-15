1511b3df688936f10dda920a91bbb2e04414588b
Gonzalo H. Velazco <gonzalovelazco32@gmail.com>
2022-04-15 12:31:49 -0300

    Second point of exercice 3 done.

--- ejercicio3.py
+++ ejercicio3.py
@@ -13,10 +13,11 @@ def run():
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 #First point of the exercice.
+    new_list = []
     for i in a:
         if i < 5:
-            print(i)
-
+            new_list.append(i)
+    print(new_list)
 
 if __name__ == '__main__':
     run()
