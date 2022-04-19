e00ab649d74090145872faa23aa5fb91c0fc26ff
Gonzalo H. Velazco <gonzalovelazco32@gmail.com>
2022-04-19 19:18:10 -0300

    Doing the first extra task

---
+++ exercise14_1.py
@@ -0,0 +1,31 @@
+# Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+
+# Extras:
+# Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+# Go back and do Exercise 5 using sets, and write the solution for that in a different function.
+
+
+import random
+
+def run():
+    counter_1 = 0
+    counter_2 = 0
+    l_list = []
+    m_list = []
+    
+    def list_generator(choose_counter, count_level, append_list):
+        while choose_counter < count_level:
+            generator = random.randint(1, 101)
+            append_list.append(generator)
+            choose_counter += 1
+
+    list_generator(counter_1, 20, l_list)
+    print(l_list)
+
+    
+
+    #finall_list = [i for i in l_list if ]
+    #print(finall_list)
+
+if __name__ == '__main__':
+    run()
